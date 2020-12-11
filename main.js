90c1b9b184ea6ac795154afee5e6c90415cecc63
Anwesha Priyam <72214354+Anwesha-Priyam@users.noreply.github.com>
2020-12-11 18:33:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var canvas= new fabric.Canvas("Canvas");
+block_width=30;
+block_height=30;
+player_x=500;
+player_y=700;
+player_object="MCPE Player.jpeg";
+block_object="";
+function PUpdate()
+{
+    new Image(player_object);
+}
